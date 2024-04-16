357b01ccd1605e391f457e9632b67f749cd7cd27
GuyShinto <xiiioffice000@gmail.com>
2024-04-16 18:23:32 +0700

    5

--- scripts/melt.gd
+++ scripts/melt.gd
@@ -7,13 +7,18 @@ extends Node
 @export var max_malt :float = 10
 
 var baseScale :Vector2 = Vector2(1,1)
+var baseMass :float
 
 func _ready():
+	if main is RigidBody2D:
+		baseMass = main.mass
 	timer.wait_time = max_malt
 	timer.timeout.connect(_on_timeout)
 	timer.start()
 
 func _physics_process(_delta):
+	if main is RigidBody2D:
+		main.mass = clamp(baseMass * (timer.time_left/max_malt),1,100)
 	main.scale = clamp(baseScale * (timer.time_left/max_malt),baseScale*0.1,baseScale)
 	collision.scale = clamp(baseScale * (timer.time_left/max_malt),baseScale*0.1,baseScale)
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,9 @@
 extends RigidBody2D
 
+@export var speed = 50
+
 func _ready():
 	Global.player = self
+
+func _physics_process(_delta):
+	self.angular_velocity = Input.get_axis("left", "right") * speed * _delta
